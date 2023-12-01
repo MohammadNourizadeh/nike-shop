dfe3e29f9f2781975de81ae01a7f61a550e5bbb8
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2023-12-01 19:00:22 +0330

    some elements removed

--- assets/JS/nike-shop.js
+++ assets/JS/nike-shop.js
@@ -1,64 +1,3 @@
-const iconNum = document.getElementById('added-product-number');
-const navIconDiv = document.getElementById('navbar-icon-div');
-
-// products count
-let totalNum = 0;
-let shoe1 = 0;
-let shoe2 = 0;
-let shoe3 = 0;
-let shoe4 = 0;
-let shoe5 = 0;
-let shoe6 = 0;
-let shoe7 = 0;
-let shoe8 = 0;
-let shoe9 = 0;
-let shoe10 = 0;
-let shoe11 = 0;
-let shoe12 = 0;
-
-// cards adding btns
-const addBtn1 = document.getElementById('product-add-btn1');
-const addBtn2 = document.getElementById('product-add-btn2');
-const addBtn3 = document.getElementById('product-add-btn3');
-const addBtn4 = document.getElementById('product-add-btn4');
-const addBtn5 = document.getElementById('product-add-btn5');
-const addBtn6 = document.getElementById('product-add-btn6');
-const addBtn7 = document.getElementById('product-add-btn7');
-const addBtn8 = document.getElementById('product-add-btn8');
-const addBtn9 = document.getElementById('product-add-btn9');
-const addBtn10 = document.getElementById('product-add-btn10');
-const addBtn11 = document.getElementById('product-add-btn11');
-const addBtn12 = document.getElementById('product-add-btn12');
-
-// cards removing btns
-const removeBtn1 = document.getElementById('product-remove-btn1');
-const removeBtn2 = document.getElementById('product-remove-btn2');
-const removeBtn3 = document.getElementById('product-remove-btn3');
-const removeBtn4 = document.getElementById('product-remove-btn4');
-const removeBtn5 = document.getElementById('product-remove-btn5');
-const removeBtn6 = document.getElementById('product-remove-btn6');
-const removeBtn7 = document.getElementById('product-remove-btn7');
-const removeBtn8 = document.getElementById('product-remove-btn8');
-const removeBtn9 = document.getElementById('product-remove-btn9');
-const removeBtn10 = document.getElementById('product-remove-btn10');
-const removeBtn11 = document.getElementById('product-remove-btn11');
-const removeBtn12 = document.getElementById('product-remove-btn12');
-
-// products counter that can be seen on cards
-const counter1 = document.getElementById('counter1');
-const counter2 = document.getElementById('counter2');
-const counter3 = document.getElementById('counter3');
-const counter4 = document.getElementById('counter4');
-const counter5 = document.getElementById('counter5');
-const counter6 = document.getElementById('counter6');
-const counter7 = document.getElementById('counter7');
-const counter8 = document.getElementById('counter8');
-const counter9 = document.getElementById('counter9');
-const counter10 = document.getElementById('counter10');
-const counter11 = document.getElementById('counter11');
-const counter12 = document.getElementById('counter12');
-
-
 // product specifications
 const product = [{
     id: 1,
@@ -141,7 +80,7 @@ product.map(item => {
     productCol.innerHTML = `
         <div class="card h-100 shadow">
          <div class="product-img-div position-relative">
-             <small id="counter1" class="position-absolute end-0 h5 d-none"></small>
+             <small class="position-absolute end-0 h5 d-none"></small>
              <img src=${item.img} class="card-img-top" alt="...">
          </div>
          <div class="card-body">
